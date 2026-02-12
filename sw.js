0e2449525f244c5b972b4785c215c57791871322
fmffmffk-rgb <fmffmffk@gmail.com>
2026-02-12 12:54:35 +0100

    Create sw.js
    G

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('push', e => {
+  const data = e.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: 'icon.png',
+    data: { url: data.url }
+  });
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow(e.notification.data.url));
+});
